e877e553bba9c0e8f682adf63292d2263a1d5b80
Manik Gupta <manikgupta919@gmail.com>
2023-08-23 12:49:28 +0530

    added filter for 30mins

--- q1/index.js
+++ q1/index.js
@@ -1,7 +1,26 @@
 var express = require('express');
+const axios = require("axios");
+
 var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/getSchedule', async function (req, res) {
+
+  try {
+    const access_token = await axios.post("http://20.244.56.144/train/auth", {
+      "companyName": "2000971540035",
+      "clientID": "8885e728-f90a-491f-bf41-674744ec2013",
+      "clientSecret": "EDnTAfUSllforGFt",
+      "ownerName": "Manik Gupta",
+      "ownerEmail": "manikgupta919@gmail.com",
+      "rollNo": "2000971540035"
+      });
+    const token = access_token.data.access_token
+    const scheduleRes = await axios.get("http://20.244.56.144/train/trains", { headers: {"Authorization" : `Bearer ${token}`}})
+    const schedule = scheduleRes.data
+    res.send(schedule)
+  } catch(e) {
+
+  }
+  
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
